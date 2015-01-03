9933c777eb46d1d047c44af3ea77f72eb3462cda
Sicknemesis <hulluhalonen10@gmail.com>
2015-01-03 19:54:14 +0200

    Fix toolchain info for my sabermod toolchain
    Change-Id: Iea95eb9ad76fdbd1641b661af431d74d7b0f469e

--- config/sicki.mk
+++ config/sicki.mk
@@ -27,15 +27,15 @@ GCC_VERSION_AND := 4.8
 SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(GCC_VERSION_AND)
 SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
 
-ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_AND)),)
+ifneq ($(filter (SM-Toolchain) (Sicki-SaberMod%),$(SM_AND)),)
 # GCC Colors only works on gcc >=4.9.x
 $(shell `export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'`)
 SM_AND_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(SM_AND))
-SM_AND_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_AND))
+SM_AND_NAME := $(filter (SM-Toolchain) (Sicki-SaberMod%),$(SM_AND))
 SM_AND_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_AND))
 SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
 
-ifeq ($(filter (SaberMod%),$(SM_AND)),)
+ifeq ($(filter (Sicki-SaberMod%),$(SM_AND)),)
 SM_AND_VERSION := $(SM_AND_NAME)_$(SM_AND_VERSION)_$(SM_AND_DATE)-$(SM_AND_STATUS)
 else
 SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
@@ -54,15 +54,15 @@ GCC_VERSION_ARM := 4.9
 SM_ARM_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(GCC_VERSION_ARM)
 SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
 
-ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_ARM)),)
+ifneq ($(filter (SM-Toolchain) (Sicki-SaberMod%),$(SM_ARM)),)
 # GCC Colors only works on gcc >=4.9.x
 export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 SM_ARM_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(SM_ARM))
-SM_ARM_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_ARM))
+SM_ARM_NAME := $(filter (SM-Toolchain) (Sicki-SaberMod%),$(SM_ARM))
 SM_ARM_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_ARM))
 SM_ARM_STATUS := $(filter (release) (prerelease) (experimental),$(SM_ARM))
 
-ifeq ($(filter (SaberMod%),$(SM_ARM)),)
+ifeq ($(filter (Sicki-SaberMod%),$(SM_ARM)),)
 SM_ARM_VERSION := $(SM_ARM_NAME)_$(SM_ARM_VERSION)_$(SM_ARM_DATE)-$(SM_ARM_STATUS)
 else
 SM_ARM_VERSION := $(SM_ARM_NAME)-$(SM_ARM_DATE)-$(SM_ARM_STATUS)
